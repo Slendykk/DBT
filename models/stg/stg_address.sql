7470d1b70dbaf62b8bfa0b9682c171f282eec11f
sjcsong@g.ucla.edu <sjcsong@g.ucla.edu>
2022-03-01 05:06:09 +0000

    mode

--- models/stg/stg_address.sql
+++
@@ -1,5 +0,0 @@
-{{ config(materialized='table') }}
-
-select * from {{ source('mysql_sakila','address') }}
-where _FIVETRAN_DELETED = FALSE
-
--- models/stg/stg_staff.sql
+++
@@ -1,5 +0,0 @@
-{{ config(materialized='table') }}
-
-select * from {{ source('mysql_sakila','staff') }}
-where _FIVETRAN_DELETED = FALSE
-
